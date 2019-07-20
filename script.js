7511a8aefde1ea8791868c6a29612ef7c7ad3684
aline-bebe <ingabirealine40@gmail.com>
2019-07-20 19:16:01 +0200

    get date from js

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function weekdays () {
-  var day = document.getElementById("day").value;
-  var enterDate = day.split("-");
-  var dd = parseInt(enterDate[2]);
-  var mm = parseInt(enterDate[1]);
-  var yy = parseInt(enterDate[0]);
+var theMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-   var CC = (yy - 1) / 100 + 1;
-   var birthDay = (CC / 4 - 2 * CC - 1 + 5 * yy / 4 + 26 * (mm + 1) / 10 + dd) % 7;
- 
-document.getElementById("display").innerHTML=Math.round(birthDay);
-var malesNames = ["kwasi", "kwadwo","kwabena", "kwaku", "yaw", "kofi", "kwame"];
-var femalesNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var theFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var gender = document.querySelector('input[name="optradio"]:checked').value;
- console.log(gender);
+function thedate() {
+    
+    var input = document.getElementById("input");
 
- if (gender=="male"){
-   console.log(malesNames[Math.round(birthDay)]);
-   document.getElementById("names").innerHTML = malesNames[Math.round(birthDay)];
- }else if(gender=="female"){
-  console.log(femalesNames[Math.round(birthDay)]);
-   document.getElementById("names").innerHTML = femalesNames[Math.round(birthDay)];
- }else {
-   console.log("invalid message");
- }
+    var M_Gender = document.getElementById("Mgender");
+    var F_Gender = document.getElementById("Fgender");
+
+    var date = new Date(input.value);
+    var weekday = date.getDay();
+
+    var names = document.getElementById("names");
+    var theWeekDay = document.getElementById("theWeekDay");
+
+    if(M_Gender.checked){
+        for(let i=0; i<weekdays.length; i++){
+            
+            if(weekdays[weekday] == weekdays[i]){
+
+                theWeekDay.innerHTML = weekdays[i];
+                names.innerHTML = theMale[i];
+
+            }
+        }
+    }
+
+    if(F_Gender.checked){
+        for(let i=0; i<weekdays.length; i++){
+            
+            if(weekdays[weekday] == weekdays[i]){
+
+                theWeekDay.innerHTML = weekdays[i];
+                names.innerHTML = theFemale[i];
+
+            }
+        }
+    }
+
 }
- 
-
-
